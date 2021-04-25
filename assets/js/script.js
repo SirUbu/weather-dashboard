21752a709d7da3466bc0d96a4ac9fc7a4e469000
Sir Ubu <thesirubu@gmail.com>
2021-04-24 19:08:48 -0600

    Finished fetch api.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,40 @@
 var currentWeatherEl = $("#currentWeather");
 
 // function to fetch api
-var citySearchAPI = function(userSearch) {
-    // update DOM
-    currentWeatherEl.text("Searching Please Wait...").addClass("bg-warning text-center p-2");
+var APISearch = function(userSearch) {
+    // update DOM that search is happening
+    currentWeatherEl.text("Searching Please Wait...").attr("class", "bg-warning text-center p-2");
+
     // store api key for reference
     var APIKey = "5ca19e2089c84836dfdfb8cf20c691ed";
 
-    // api address using userSearch and APIKey
-    var apiAddress = `https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&appid=${APIKey}`;
-
-    // make fetch using address
-    fetch(apiAddress)
-        .then(function(response) {
-            if(response.ok) {
-                return response.json();
+    // make fetch using address with userSearch
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&appid=${APIKey}`)
+        .then(function(cityResponse) {
+            if(cityResponse.ok) {
+                return cityResponse.json();
             } else {
-                currentWeatherEl.text("Your search had no results").addClass("bg-danger p-2 text-center");
+                currentWeatherEl.text(`Error ${cityResponse.status}: ${cityResponse.statusText}`).attr("class", "bg-danger p-2 text-center");
             }
         })
         // pass to display function
-        .then(function(data) {
+        .then(function(cityData) {
             // store lon and lat
-            var searchLon = data.coord.lon;
-            var searchLat = data.coord.lat;
+            var searchLon = cityData.coord.lon;
+            var searchLat = cityData.coord.lat;
 
-            // pass lon and lat to oneCallAPI
-            oneCallAPI(searchLon, searchLat);
+            // use searchLon and searchLat to fetch api of one call
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${searchLat}&lon=${searchLon}&appid=${APIKey}`)
+                .then(function(oneCallResponse) {
+                    if(oneCallResponse.ok) {
+                        return oneCallResponse.json();
+                    } else {
+                        currentWeatherEl.text("Your search had no results").attr("class", "bg-danger p-2 text-center");
+                    }
+                })
+                .then(function(oneCallData) {
+                    displayWeather(oneCallData);
+                })
         })
 };
 
@@ -42,7 +50,7 @@ $("#searchBtn").click(function(event) {
 
     var userSearch = $("#userSearch").val();
 
-    citySearchAPI(userSearch);
+    APISearch(userSearch);
 
     $("#userSearch").val("");
 });
